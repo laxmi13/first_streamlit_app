2da2214b3dbee918db3e88c55201f0596d69f1de
laxmi13 <kdhanalaxmi@gmail.com>
2022-09-28 22:10:14 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
 
-streamlit.text('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 
 streamlit.text('Omega 3 & Bluebetty Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
