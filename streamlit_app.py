e91b08bdcde8d98de128b946d42ea1c58a03b410
laxmi13 <kdhanalaxmi@gmail.com>
2022-10-06 23:35:52 -0400

    Update streamlit_app.py
    fixed the ( error.

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ streamlit.dataframe(fruits_to_show)
 #displays fruityvice api response
 streamlit.header('Fruitvice Fruit Advice!')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-fruityvice_normalized = pandas.json_normalizefruityvice_response.json())
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 
