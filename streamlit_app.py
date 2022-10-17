2c24cce8ab8099eae0987f051282765baffcf3b2
laxmi13 <kdhanalaxmi@gmail.com>
2022-10-17 12:23:35 -0400

    Update streamlit_app.py
    added dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,10 @@ my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM fruit_load_list")
 my_data_row = my_cur.fetchone()
 #streamlit.text("Hello from snowflake")
-streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_row)
+#streamlit.text("The fruit load list contains:")
+#streamlit.text(my_data_row)
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_row)
 
 
 streamlit.title("My Mom's New Healthy Diner")
