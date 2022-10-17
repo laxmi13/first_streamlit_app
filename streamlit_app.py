77c4b8ab0d5ed483753c9faaaf89f3366f78354d
laxmi13 <kdhanalaxmi@gmail.com>
2022-10-17 16:18:50 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ streamlit.header('Fruitvice Fruit Advice!')
 
 #snowflake related functions:
 def get_fruit_load_list():
-   with my_cnx_cursor as my_cur:
+   with my_cnx.cursor() as my_cur:
          my_cur.execute("SELECT * FROM fruit_load_list")
 
 #Add a button to load the fruit
