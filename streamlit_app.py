d4bd7138e7ab039d219b1fb3af0aa284dd4cd027
laxmi13 <kdhanalaxmi@gmail.com>
2022-10-17 14:21:13 -0400

    Update streamlit_app.py
    stopping streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,7 @@ streamlit.dataframe(my_data_rows)
 
 #new section to display fruityvice api response
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+streamlit.stop();
 streamlit.write('The user entered', add_my_fruit)
 
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
