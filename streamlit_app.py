defc482d38e0ab596d29fe888bf2ee1605dd038b
laxmi13 <kdhanalaxmi@gmail.com>
2022-10-21 10:02:53 -0400

    Update streamlit_app.py
    testing #snowflake related functions:
    def get_fruit_load_list():
       with my_cnx.cursor() as my_cur:
             my_cur.execute("SELECT * FROM fruit_load_list")
             return my_cur.fetchall()

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,16 +10,17 @@ streamlit.title("My Mom's New Healthy Diner")
 #new section to display fruityvice api response
 streamlit.header('Fruitvice Fruit Advice!')
 
+#snowflake related functions:
+def get_fruit_load_list():
+   with my_cnx.cursor() as my_cur:
+         my_cur.execute("SELECT * FROM fruit_load_list")
+         return my_cur.fetchall()
+      
 #Add a button to load the fruit
 if streamlit.button('Get fruit load list'):
      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
      my_data_rows = get_fruit_load_list()
      streamlit.dataframe(my_data_rows)
       
-#snowflake related functions:
-def get_fruit_load_list():
-   with my_cnx.cursor() as my_cur:
-         my_cur.execute("SELECT * FROM fruit_load_list")
-         return my_cur.fetchall()
       
 
