f04e7cbb374b5dd2f6f95718178015f4e29124b0
laxmi13 <kdhanalaxmi@gmail.com>
2022-10-21 10:31:54 -0400

    Update streamlit_app.py
    replaced a parameter in a function insert_row_snowflake()

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,12 +19,10 @@ def get_fruit_load_list():
 #snowflake related functions:
 def insert_row_snowflake(new_fruit):
    with my_cnx.cursor() as my_cur:
-         #my_cur.execute("SELECT * FROM fruit_load_list")
-         my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+         my_cur.execute("insert into fruit_load_list values ('"+ new_fruit +"')")
          return "Thanks for adding " + new_fruit
       
-#Add a button to load the fruit
-if streamlit.button('Get fruit load list'):
+#Add a button to load the fruitif streamlit.button('Get fruit load list'):
      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
      my_data_rows = get_fruit_load_list()
      streamlit.dataframe(my_data_rows)
@@ -36,8 +34,8 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a fruit to the list'):
      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
      back_from_function = insert_row_snowflake(add_my_fruit)
-     #streamlit.dataframe(back_from_function)
-     streamlit.write(back_from_function)
+     streamlit.dataframe(back_from_function)
+     
       
 
 
